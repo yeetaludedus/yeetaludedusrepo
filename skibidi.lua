9fe917376bd0891520e475f20d7db02047c9eeca
yeetaludedus <161526671+yeetaludedus@users.noreply.github.com>
2025-02-10 19:05:29 -0700

    Update skibidi.lua

--- skibidi.lua
+++ skibidi.lua
@@ -1,1 +1,4 @@
-print("hi")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+if game.PlaceId == 155615604 then
+    print("passed check")
+end
